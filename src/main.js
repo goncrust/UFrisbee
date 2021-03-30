eb768fbb1425c3208a3c844a36d41f2c0cc614df
goncrust <goncalonrua@gmail.com>
2021-03-30 18:53:58 +0100

    starter project

---
+++ src/main.js
@@ -0,0 +1,35 @@
+let canvas = document.getElementById("display");
+let ctx = canvas.getContext("2d");
+
+document.onload = () => {
+    init();
+}
+
+
+
+// initial function
+function init() {
+
+
+    window.requestAnimationFrame(mainloop)
+}
+
+function mainloop() {
+    // mainloop
+
+    logic();
+
+    render();
+
+    window.requestAnimationFrame(mainloop);
+}
+
+// render graphics
+function render() {
+
+}
+
+// calculate logic
+function logic() {
+
+}
